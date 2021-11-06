f0150815e054ae660dc32d913b9924e9590d58ec
Wang <3428141843@qq.com>
2021-11-06 15:17:01 +0800

    [KotlinDemo][1/1]{模块间api打jar包提供给其他模块使用gradle脚本；} 适用范围：{All} 准入id：{无} 分析：{无} 方案：{使用方法：在需要打包对外提供api或者数据bean的模块gradle，添加引用根目录下的”generate_api.gradle“，然后执行gragle中publishing下的”publishXxxPublicationToMavenRepository“命令，即可打包成功，在需要引用该jar包的地方，添加该jar包的依赖即可；例子可见本次提交中的account模块引用示例} 风险及影响：{无} 测试建议：{} 其它：{}

---
+++ account/src/main/java/com/ngb/kotlindemo/account/_pojo/UserBean.kt
@@ -0,0 +1,7 @@
+package com.ngb.kotlindemo.account._pojo
+
+data class UserBean(var userId: String,
+                    var userName: String,
+                    var registerTime: Long,
+                    var description: String,
+                    var portraitUrl: String)
